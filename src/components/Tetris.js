ba22757b8b80dde245f89467a6aee7847b903386
Paul Borawski <paulborawski14@gmail.com>
2019-08-21 18:43:08 -0400

    more scaffolding

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
 
-class Tetris extends Component {
-  render() {
-    return (
-      <div>Hello</div>
-    )
-  }
+const Tetris = () => {
+  return (
+    <div>
+      <Stage />
+      <aside>
+        <div>
+          <Display text="Score" />
+          <Display text="Rows" />
+          <Display text="Level" />
+        </div>
+        <StartButton />
+      </aside>
+    </div>
+  )
 }
 
 export default Tetris;

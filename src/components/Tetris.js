bdcdb6b3d4357182df6730cb01c8ae06607ef3b0
Paul Borawski <paulborawski14@gmail.com>
2019-08-21 19:11:26 -0400

    fixed path

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStage } from '../gameHelpers';
+import { createStage } from './gameHelpers';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
